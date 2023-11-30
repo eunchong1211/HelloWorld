579a4a4a14efa60548ed682011f3329ef240d4c9
eunchong1211 <113576934+eunchong1211@users.noreply.github.com>
2023-11-30 12:13:33 +0900

    Update HelloGitWorld.java

--- HelloGitWorld.java
+++ HelloGitWorld.java
@@ -1,5 +1,6 @@
 public class HelloGitWorld{
 	public static void main(Sting[] args){
-		System.out.println("Git Test");	
+		System.out.println("Git Test");
+		System.out.println("GitHub에서 수정해봄");
 	}
 }
